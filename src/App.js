e90aaabf685fe69e74f15d5aab61ae44c1f2dc90
LinasB <111748062+lencikas@users.noreply.github.com>
2022-11-27 19:02:44 +0200

    pirmieji pakeitimai

--- src/App.js
+++ src/App.js
@@ -5,50 +5,74 @@ import Homepage from './pages/Homepage'
 import Navigation from './components/Navigation'
 import Page2 from './pages/page2'
 import Page3 from './pages/page3'
+import Meal from './components/meal'
 
 function App() {
 
-  const [quote, setQuote] = useState('Hello');
-  const [author, setAuthor] = useState('World');
+  // ( PIRMOJO PROJEKTO DALIS
+  // const [quote, setQuote] = useState('Hello');
+  // const [author, setAuthor] = useState('World');
 
-  useEffect(() => {
-    fetch('http://api.quotable.io/random')
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content)
-          setAuthor(quote.author)
-        }
-      )
-  },[])
+  // useEffect(() => {
+  //   fetch('http://api.quotable.io/random')
+  //     .then(res => res.json())
+  //     .then(
+  //       (quote) => {
+  //         setQuote(quote.content)
+  //         setAuthor(quote.author)
+  //       }
+  //     )
+  // }, [])
 
-  let fetchNewQuote = () => {
-    fetch('http://api.quotable.io/random')
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content)
-          setAuthor(quote.author)
-        }
-      )
-  }
+  // let fetchNewQuote = () => {
+  //   fetch('http://api.quotable.io/random')
+  //     .then(res => res.json())
+  //     .then(
+  //       (quote) => {
+  //         setQuote(quote.content)
+  //         setAuthor(quote.author)
+  //       }
+  //     )
+  // }
+  // )
+
 
   return (
-    <div className='App'>
-      <div className='quote'>
-        <h2>{quote}</h2>
-        <small>{author}</small>
+    <>
+      <div>
+        <h1 className="font-bold text-4xl lg:text-6xl mt-10">
+          Random meal generator
+        </h1>
+        <div className="underline mx-auto mt-4 mb-5"></div>
+        <Meal />
       </div>
-      <button className='btn' onClick={fetchNewQuote}>Generate New Quote</button>
+
+
+
+
+
+
+
+
+
+
 
       {/* <Navigation />
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/page2' element={<Page2 />} />
-        <Route path='/page3' element={<Page3/>} />
+        <Route path='/page3' element={<Page3 />} />
         <Route path='*' element={<h1>Page not found</h1>} />
       </Routes> */}
-    </div>
+
+      {/* ( PIRMOJO PROJEKTO DALIS
+      <div className='quote'>
+        <h2>{quote}</h2>
+        <small>{author}</small>
+      </div>
+      <button className='btn' onClick={fetchNewQuote}>Generate New Quote</button>
+      ) */}
+    </>
   )
 }
 
